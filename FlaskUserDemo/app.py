5656f35a682741783491109fe4e8f7da75b14235
Lulu Li <lulli@student.pakuranga.school.nz>
2022-06-28 08:58:53 +1200

    change

--- FlaskUserDemo/app.py
+++ FlaskUserDemo/app.py
@@ -6,18 +6,25 @@ app = Flask(__name__)
 from utils import create_connection, setup
 app.register_blueprint(setup)
 
-#@app.before_request
-#def restrict():
-#    restricted_pages = [
-#        'list_users',
-#        'view_user',
-#        'edit_user',
-#        'delete_user',
-#        'add_subject'
-#        ]
-#    if 'logged_in' not in session and request.endpoint in restricted_pages:
-#        flash("You are not logged in.")
-#        return redirect('/login')
+@app.before_request
+def restrict():
+    #return abort(401)
+    #if request.endpoint != 'home':
+    restricted_pages = [
+        'list_users',
+        'view_user',
+        'edit_user', 
+        'delete_user',
+        'list_subject',
+        'delete_subject'
+        ]
+    admin_only = [
+        'list_users'
+        'list_movies'
+    ]
+    if 'logged_in' not in session and request.endpoint in restricted_pages:
+        flash("You are not logged in!")
+        return redirect('/login')
 
 @app.route('/')
 def home():
@@ -76,8 +83,8 @@ def add_user():
         with create_connection() as connection:
             with connection.cursor() as cursor:
                 sql = """INSERT INTO users 
-                    (first_name, last_name, email, password, avatar)
-                    VALUES (%s, %s, %s, %s, %s)"""
+                    (first_name, last_name, email, password, avatar, role)
+                    VALUES (%s, %s, %s, %s, %s, "Student")"""
                 values = (
                     request.form['first_name'],
                     request.form['last_name'],
@@ -91,22 +98,6 @@ def add_user():
                 except pymysql.err.IntegrityError:
                     flash('Email has already been taken.')
                     return redirect('/register')
-
-# Login in the user after they sign up and take them to their profile page
-                sql = """SELECT * FROM users WHERE email = %s AND password = %s"""
-                values = (
-                    request.form['email'],
-                    encrypted_password
-                )
-                cursor.execute(sql, values)
-                result = cursor.fetchone()
-
-            if result:
-                session['logged_in'] = True
-                session['first_name'] = result['first_name']
-                session['role'] = result['role']
-                session['user_id'] = result['user_id']
-                return redirect (url_for('view_user', user_id=session['user_id']))
 
     return render_template('users_add.html')
 
@@ -134,8 +125,8 @@ def view_user():
             result = cursor.fetchone()
     return render_template('users_view.html', result=result)
 
-@app.route('/watched')
-def choosen_subject():
+@app.route('/chosen')
+def chosen_subject():
     with create_connection() as connection:
         with connection.cursor() as cursor:
             sql = """SELECT * FROM users
@@ -147,7 +138,7 @@ def choosen_subject():
                 )
             cursor.execute(sql, values)
             result = cursor.fetchall()
-    return render_template('watched_list.html', result=result)
+    return render_template('chosen_list.html', result=result)
 
 @app.route('/addsub')
 def add_subject():
@@ -165,9 +156,9 @@ def add_subject():
 # TODO: Add a '/delete_user' route that uses DELETE
 @app.route('/delete')
 def delete_user():
-    #if session['role'] != 'admin' and str(session['id']) != request.args['id']: 
-    #    flash("You don't have persmission to delete this user")
-    #    return redirect('/view?id=' + request.args['id'])
+    if session['role'] != 'admin' and str(session['user_id']) != request.args['user_id']: 
+        flash("You don't have persmission to delete this user")
+        return redirect('/view?id=' + request.args['user_id'])
     with create_connection() as connection:
             with connection.cursor() as cursor:
                 cursor.execute("DELETE FROM users WHERE user_id=%s", request.args['id'])
@@ -182,14 +173,14 @@ def delete_subject():
             with connection.cursor() as cursor:
                 cursor.execute("DELETE FROM users_subject WHERE subject_id=%s", request.args['subject_id'])
                 connection.commit()
-    return redirect ('/choosen?user_id=' + str(session['user_id']))
+    return redirect ('/chosen?user_id=' + str(session['user_id']))
 
 @app.route('/edit', methods=['GET', 'POST'])
 def edit_user():
-    ## Admin are allowed, users with the right id are allowed, everyone else sees 404.
-    #if session['role'] != 'admin' and str(session['user_id']) != request.args['id']:
-    #    flash("You don't have permission to edit this user.")
-    #    return redirect('/view?user_id=' + request.args['id'])
+    # Admin are allowed, users with the right id are allowed, everyone else sees 404.
+    if session['role'] != 'admin' and str(session['user_id']) != request.args['user_id']:
+        flash("You don't have permission to edit this user.")
+        return redirect('/view?user_id=' + request.args['user_id'])
 
     if request.method == 'POST':
         if request.files['avatar'].filename:
@@ -229,20 +220,20 @@ def edit_user():
                 result = cursor.fetchone()
         return render_template('users_edit.html', result=result)
 
-#@app.route('/checkemail')
-#def check_email():
-#    with create_connection() as connection:
-#        with connection.cursor() as cursor:
-#            sql = "SELECT * FROM users WHERE email = %s"
-#            values = (
-#                request.args['email']
-#            )
-#            cursor.execute(sql, values)
-#            result = cursor.fetchone()
-#    if result:
-#        return jsonify({ 'status': 'Error' })
-#    else:
-#        return jsonify({ 'status': 'OK' })
+@app.route('/checkemail')
+def check_email():
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM users WHERE email = %s"
+            values = (
+                request.args['email']
+            )
+            cursor.execute(sql, values)
+            result = cursor.fetchone()
+    if result:
+        return jsonify({ 'status': 'Error' })
+    else:
+        return jsonify({ 'status': 'OK' })
 
 if __name__ == '__main__':
     import os
